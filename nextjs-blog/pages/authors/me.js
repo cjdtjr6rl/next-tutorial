8a82e652b86cc5166bfa9a8b71a766657030d647
front-junhyoung <junhyoung@stamper.network>
2021-08-08 13:42:16 +0900

    study next.js in Assets, Metadata, and CSS

--- nextjs-blog/pages/authors/me.js
+++ nextjs-blog/pages/authors/me.js
@@ -1,3 +1,17 @@
-export default function Me () {
-  return <h1>I am Junnna.</h1>
+import React from 'react';
+import Image from 'next/image';
+import profilePic from '../images/profile.jpg';
+
+export default function Me() {
+  return (
+    <>
+      <h1>Hi I am Junnna.</h1>
+      <Image
+        src={profilePic}
+        height={144}
+        width={144}
+        alt="My Avatar"
+      />
+    </>
+  );
 }
